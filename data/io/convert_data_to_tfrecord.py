66e49515cfcb8a2c6dd978a89ca9065976972280
yangxue0827 <yangxue0827@126.com>
2017-12-16 15:49:06 +0800

    Update

--- data/io/convert_data_to_tfrecord.py
+++ data/io/convert_data_to_tfrecord.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, absolute_import
 import sys
-sys.path.append('../')
+sys.path.append('../../')
 import xml.etree.cElementTree as ET
 from libs.configs import cfgs
 import numpy as np
